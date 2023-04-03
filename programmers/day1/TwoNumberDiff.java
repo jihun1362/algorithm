47e08ab4675e5285624153026fb472f265a94cd1
jihunback <jihun1362@nate.com>
2023-04-03 16:03:18 +0900

    day1 두수의 합 두수의 차 두수의 곱 두수의 몫

---
+++ programmers/day1/TwoNumberDiff.java
@@ -0,0 +1,15 @@
+package programmers.day1;
+
+class Solution2 {
+    public static int solution(int num1, int num2) {
+        int answer;
+        answer = num1 - num2;
+        return answer;
+    }
+}
+
+public class TwoNumberDiff {
+    public static void main(String[] args) {
+        System.out.println(Solution2.solution(1, 2));
+    }
+}
---
+++ programmers/day1/TwoNumberShare.java
@@ -0,0 +1,15 @@
+package programmers.day1;
+
+class Solution4 {
+    public static int solution(int num1, int num2) {
+        int answer;
+        answer = num1 / num2;
+        return answer;
+    }
+}
+
+public class TwoNumberShare {
+    public static void main(String[] args) {
+        System.out.println(Solution4.solution(10, 5));
+    }
+}
