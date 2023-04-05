67e2d11f744fe4a36590cfe6d827ce60320c8ed1
jihunback <jihun1362@nate.com>
2023-04-05 09:11:50 +0900

    day2 두수의 나눗셈

---
+++ programmers/day2/TwoNumberDivision.java
@@ -0,0 +1,12 @@
+package programmers.day2;
+
+public class TwoNumberDivision {
+
+    public static int solution(int num1, int num2) {
+        return (int)(num1/(double)num2 * 1000);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(10, 2));
+    }
+}
