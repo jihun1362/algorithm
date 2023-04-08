cbbd6eb9b71ee6ec64c328894f4fbe8e78838be2
jihunback <jihun1362@nate.com>
2023-04-09 03:17:15 +0900

    day2 분수 더하기

---
+++ programmers/codingTestBeginner/day2/ArrayDouble.java
@@ -0,0 +1,20 @@
+package programmers.codingTestBeginner.day2;
+
+import java.util.Arrays;
+
+public class ArrayDouble {
+
+    public static int[] solution(int[] numbers) {
+        // int[] answer = new int[numbers.length];
+        // for(int i=0;i<numbers.length;i++){
+        //     answer[i] = numbers[i] * 2;
+        // }
+        for (int i = 0; i < numbers.length; i++) numbers[i] *= 2;
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        int[] a={1,2,3,4};
+        System.out.println(Arrays.toString(solution(a)));
+    }
+}
