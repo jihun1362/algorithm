1429f11c38100f3d49cd86a5d544e185bb3acbc2
jihunback <jihun1362@nate.com>
2023-04-10 08:46:03 +0900

    day3 나머지 구하기

---
+++ programmers/codingTestBeginner/day3/NamuageGuhagi.java
@@ -0,0 +1,11 @@
+package programmers.codingTestBeginner.day3;
+
+public class NamuageGuhagi {
+    public static int solution(int num1, int num2) {
+        return num1%num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(3,2));
+    }
+}
