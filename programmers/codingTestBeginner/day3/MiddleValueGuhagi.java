d93057bd75bbb6c9a6d7b42f467c7d89150ad6f1
jihunback <jihun1362@nate.com>
2023-04-11 09:12:43 +0900

    day3 중앙값구하기

---
+++ programmers/codingTestBeginner/day3/MiddleValueGuhagi.java
@@ -0,0 +1,14 @@
+package programmers.codingTestBeginner.day3;
+
+import java.util.Arrays;
+
+public class MiddleValueGuhagi {
+    public static int solution(int[] array) {
+        Arrays.sort(array);
+        return array[array.length / 2];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 10, 2, 11, 7}));
+    }
+}
