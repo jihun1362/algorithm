890de3f7335927db9b1916a48ec367c90b9e4a72
jihunback <jihun1362@nate.com>
2023-04-25 09:04:33 +0900

    day5 나이 구하기

---
+++ algorithm/programmers/codingTestBeginner/day5/AgeGuhagi.java
@@ -0,0 +1,11 @@
+package algorithm.programmers.codingTestBeginner.day5;
+
+public class AgeGuhagi {
+    public static int solution(int age) {
+        return 2023-age;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(40));
+    }
+}
