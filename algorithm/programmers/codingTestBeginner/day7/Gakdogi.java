dbc9468e3217ebe0323322cd9b900e61bbfe0164
jihunback <jihun1362@nate.com>
2023-05-05 11:27:34 +0900

    day7 각도기

---
+++ algorithm/programmers/codingTestBeginner/day7/Gakdogi.java
@@ -0,0 +1,21 @@
+package algorithm.programmers.codingTestBeginner.day7;
+
+public class Gakdogi {
+    public static int solution(int angle) {
+        int answer = 0;
+        if(0 < angle && angle < 90){
+            answer = 1;
+        }else if(angle==90){
+            answer = 2;
+        }else if(90 < angle && angle < 180){
+            answer = 3;
+        }else if(angle==180){
+            answer = 4;
+        }else answer = 0;
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(70));
+    }
+}
