02d06b79b9089ed70141e183bacbc37616f21406
jihunback <jihun1362@nate.com>
2023-05-08 08:50:07 +0900

    day7 양꼬치

--- algorithm/programmers/codingTestBeginner/day7/YangGGoChi.java
+++ algorithm/programmers/codingTestBeginner/day7/YangGGoChi.java
@@ -4,7 +4,6 @@ public class YangGGoChi {
     public static int solution(int n, int k) {
         return ((n*12000)+(k*2000)-((n/10)*2000));
     }
-
     public static void main(String[] args) {
         System.out.println(solution(10,3));
     }
