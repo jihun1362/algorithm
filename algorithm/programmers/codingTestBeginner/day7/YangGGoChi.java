d9898420e5c08674304374b0d18588cbd5094cec
jihunback <jihun1362@nate.com>
2023-05-08 08:49:04 +0900

    day7 각도기

---
+++ algorithm/programmers/codingTestBeginner/day7/YangGGoChi.java
@@ -0,0 +1,11 @@
+package algorithm.programmers.codingTestBeginner.day7;
+
+public class YangGGoChi {
+    public static int solution(int n, int k) {
+        return ((n*12000)+(k*2000)-((n/10)*2000));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(10,3));
+    }
+}
